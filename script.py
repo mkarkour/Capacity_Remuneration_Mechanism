1cb76dcc5570de077d4ce709b80d3129193a544c
Mehdi-Ulb <83882339+Mehdi-Ulb@users.noreply.github.com>
2024-08-02 22:16:48 +0200

    adding new data

---
+++ script.py
@@ -0,0 +1,9 @@
+from typing import List, Dict, Optional, Union, Any
+
+
+class Utils:
+    def __init__(self):
+        self.partition = partition
+
+    def update_complex(self,n) -> None:
+        return None
